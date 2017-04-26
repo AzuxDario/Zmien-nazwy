e81646e3493cc416e2e666a5827d3b9b843d221e
AzuxDario <pawel_mibor95@tlen.pl>
2017-04-26 23:11:38 +0200

    Dodanie nazwy i wersji aplikacji

--- Usun_podkreslenia/main.cpp
+++ Usun_podkreslenia/main.cpp
@@ -6,12 +6,14 @@ int main(int argc, char *argv[])
 {
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling); //Dostępne od Qt 5.6
     QApplication a(argc, argv);
+    a.setApplicationName("Zmień nazwy");
+    a.setApplicationVersion("3.4");
     Widget w;
     w.setWindowTitle(QString::fromUtf8("Zmień nazwy"));
     w.setMinimumSize(420,397);
     w.setMaximumSize(420,397);
     w.setWindowIcon(QIcon(":/ikona/ikona"));
-    w.move(QApplication::desktop()->screen()->rect().center() - w.rect().center()); //Przesuń na pozycję środek monitora odjąćśrodek okna
+    w.move(QApplication::desktop()->screen()->rect().center() - w.rect().center()); //Przesuń na pozycję środek monitora odjąć środek okna
     w.show();
     return a.exec();
 }
