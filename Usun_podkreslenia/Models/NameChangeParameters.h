c1eef549bf552da7ae10e7d872b84c3741de0f57
AzuxDario <pawel_mibor95@tlen.pl>
2017-11-01 22:18:50 +0100

    Changes in parameters

--- Usun_podkreslenia/Models/NameChangeParameters.h
+++ Usun_podkreslenia/Models/NameChangeParameters.h
@@ -1,6 +1,7 @@
 #ifndef NAMECHANGEPARAMETERS_H
 #define NAMECHANGEPARAMETERS_H
 
+#include <QStringList>
 
 class NameChangeParameters
 {
@@ -22,21 +23,23 @@ private:
     bool removeSpacesAtEnd;
     Letters changeLetters;
     Extensions changeExtension;
+    QStringList extensions;
 
 public:
     NameChangeParameters();
-    void setReplaceInSubfolders(bool value) noexcept {replaceInSubfolders = value;}
-    void setReplaceUnderscores(bool value) noexcept {replaceUnderscores = value;}
-    void setReplaceDashes(bool value) noexcept {replaceDashes = value;}
-    void setDontReplaceDashesSurrondedBySpaces(bool value) noexcept {dontReplaceDashesSurrondedBySpaces = value;}
-    void setReplaceDots(bool value) noexcept {replaceDots = value;}
-    void setReplaceExtensionDot(bool value) noexcept {replaceExtensionDot = value;}
-    void setReplacePluses(bool value) noexcept {replacePluses = value;}
-    void setRemoveMultiplySpaces(bool value) {removeMultiplySpaces = value;}
-    void setRemoveSpacesAtBegin(bool value) {removeSpacesAtBegin = value;}
-    void setRemoveSpacesAtEnd(bool value) {removeSpacesAtEnd = value;}
-    void setChangeLetters(Letters value) noexcept {changeLetters = value;}
-    void setChangeExtension(Extensions value) noexcept {changeExtension = value;}
+    void setReplaceInSubfolders(const bool value) noexcept {replaceInSubfolders = value;}
+    void setReplaceUnderscores(const bool value) noexcept {replaceUnderscores = value;}
+    void setReplaceDashes(const bool value) noexcept {replaceDashes = value;}
+    void setDontReplaceDashesSurrondedBySpaces(const bool value) noexcept {dontReplaceDashesSurrondedBySpaces = value;}
+    void setReplaceDots(const bool value) noexcept {replaceDots = value;}
+    void setReplaceExtensionDot(const bool value) noexcept {replaceExtensionDot = value;}
+    void setReplacePluses(const bool value) noexcept {replacePluses = value;}
+    void setRemoveMultiplySpaces(const bool value) {removeMultiplySpaces = value;}
+    void setRemoveSpacesAtBegin(const bool value) {removeSpacesAtBegin = value;}
+    void setRemoveSpacesAtEnd(const bool value) {removeSpacesAtEnd = value;}
+    void setChangeLetters(const Letters value) noexcept {changeLetters = value;}
+    void setChangeExtension(const Extensions value) noexcept {changeExtension = value;}
+    void setExtensions(const QStringList& value) noexcept {extensions = value;}
     bool getReplaceInSubfolders() noexcept {return replaceInSubfolders;}
     bool getReplaceUnderscores() noexcept {return replaceUnderscores;}
     bool getReplaceDashes() noexcept {return replaceDashes;}
@@ -46,9 +49,10 @@ public:
     bool getReplacePluses() noexcept {return replacePluses;}
     bool getRemoveMultiplySpaces() {return removeMultiplySpaces;}
     bool getRemoveSpacesAtBegin() {return removeSpacesAtBegin;}
-    bool getRemoveSpacesAtEnd() {return removeSpacesAtEnd;}
+    bool getRemoveSpacesAtEnd() {return removeSpacesAtEnd;}    
     Letters getChangeLetters() noexcept {return changeLetters;}
     Extensions getChangeExtension() noexcept { return changeExtension;}
+    QStringList& getExtensions() noexcept {return extensions;}
 };
 
 #endif // NAMECHANGEPARAMETERS_H
